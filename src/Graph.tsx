3c102a1bbd65fb68dd6938206f0a1f8ff015c4c5
Joanne <jl7105@nyu.edu>
2022-07-06 15:47:04 -0400

    corrections to graph

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -34,13 +34,13 @@ class Graph extends Component<IProps, {}> {
   componentDidMount() {
     // Get element to attach the table from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
-
+    // setAttribute(name, value)
     elem.setAttribute('view', 'y_line');
     elem.setAttribute('column-pivots', '["stocks"]');
     elem.setAttribute('row-pivots', '["timestamp"]');
     elem.setAttribute('columns', '["top_ask_price"]');
     elem.setAttribute('aggregates', `
-      {"stock": "discount count",
+      {"stock": "distinct count",
       "top_ask_price": "avg",
       "top_bid_price": "avg",
       "timestamp": "distinct count" }`);
